a49b291fc69d229cec2af138eb646044e8042704
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2023-05-06 02:43:36 +0530

    chore:Update on countdown with interactive UI

---
+++ countdown.js
@@ -0,0 +1,60 @@
+
+// 10 minutes from now
+debugger;
+var time_in_minutes = .19;
+var current_time = Date.parse(new Date());
+var deadline = new Date(current_time + time_in_minutes * 60 * 1000);
+
+function time_remaining(endtime) {
+    debugger;
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds };
+}
+
+var timeinterval;
+function run_clock(id, endtime) {
+    debugger;
+    var clock = document.getElementById(id);
+    function update_clock() {
+        var t = time_remaining(endtime);
+        clock.innerHTML = '<b>Minutes: </b>' + (t.minutes > 9 ? t.minutes : '0' + t.minutes) + '<br><b>Seconds: </b>' + (t.seconds > 9 ? t.seconds : '0' + t.seconds);
+        if (t.total <= 0) { clearInterval(timeinterval); }
+    }
+    update_clock(); // run function once at first to avoid delay
+    timeinterval = setInterval(update_clock, 1000);
+}
+run_clock('clockdiv', deadline);
+
+
+var paused = false; // is the clock paused?
+var time_left; // time left on the clock when paused
+
+function pause_clock() {
+    debugger;
+    if (!paused) {
+        paused = true;
+        clearInterval(timeinterval); // stop the clock
+        time_left = time_remaining(deadline).total; // preserve remaining time
+    }
+}
+
+function resume_clock() {
+    debugger;
+    if (paused) {
+        paused = false;
+
+        // update the deadline to preserve the amount of time remaining
+        deadline = new Date(Date.parse(new Date()) + time_left);
+
+        // start the clock
+        run_clock('clockdiv', deadline);
+    }
+}
+
+// handle pause and resume button clicks
+document.getElementById('pause').onclick = pause_clock;
+document.getElementById('resume').onclick = resume_clock;
---
+++ style.js
@@ -0,0 +1,48 @@
+const body = document.getElementById("body").style;
+const h1 = document.getElementById("h1").style;
+const pause = document.getElementsByTagName("button")[0].style;
+const resume = document.getElementsByTagName("button")[1].style;
+const clock = document.getElementById("clockdiv").style;
+
+body.textAlign = "center";
+
+h1.backgroundColor = "darkred";
+h1.color = "white";
+h1.fontFamily = "Arial";
+h1.padding = "1%";
+
+pause.backgroundColor = "darkred";
+pause.color = "white";
+pause.fontSize = "150%";
+pause.width = "10vw";
+pause.height = "5vh";
+pause.cursor = "pointer";
+
+document.getElementsByTagName("button")[0].addEventListener("click", function () {
+    pause.fontSize = "140%";
+    alert("Paused!")
+});
+
+document.getElementsByTagName("button")[0].addEventListener("mouseout", function () {
+    pause.fontSize = "150%";
+});
+
+
+resume.backgroundColor = "darkgreen";
+resume.color = "white";
+resume.fontSize = "150%";
+resume.width = "10vw";
+resume.height = "5vh";
+resume.cursor = "pointer";
+
+document.getElementsByTagName("button")[1].addEventListener("click", function () {
+    resume.fontSize = "140%";
+    alert("Resumed!")
+});
+
+document.getElementsByTagName("button")[1].addEventListener("mouseout", function () {
+    resume.fontSize = "150%";
+});
+
+clock.fontSize = "150%";
+clock.marginBottom = "2%";
